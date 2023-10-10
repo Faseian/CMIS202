45acfab067e9b4c13a546aa18136efd3e74289c6
faseian <nab4nkert@outlook.com>
2023-10-09 22:29:33 -0700

    Updates

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
 
 public class Main extends Application {
 
@@ -17,7 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("titlePage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("ScreenFiles/TitlePage.fxml"));
         Scene scene = new Scene(root, 1280, 720, Color.CORAL);
         stage.setResizable(false);
         stage.setScene(scene);

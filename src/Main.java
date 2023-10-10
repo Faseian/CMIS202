afc870a3cf4c15bed372501f21466a2b83038aa2
faseian <nab4nkert@outlook.com>
2023-10-09 22:21:02 -0700

    Merged master onto main

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class Main extends Application {
 
@@ -15,8 +17,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("ScreenFiles/TitlePage.fxml"));
-        Scene scene = new Scene(root, 1280, 720);
+        Parent root = FXMLLoader.load(getClass().getResource("titlePage.fxml"));
+        Scene scene = new Scene(root, 1280, 720, Color.CORAL);
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
